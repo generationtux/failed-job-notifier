500ab96b6d62dd26da80d12405a93214481bec5c
Lark Mullins <larkmullins@gmail.com>
2017-01-23 12:54:21 -0500

    added tests

--- tests/FailedJobNotifierServiceTest.php
+++ tests/FailedJobNotifierServiceTest.php
@@ -1,13 +1,60 @@
-<?php GenTux\FailedJobNotifier\Tests;
+<?php
 
-use GenTux\FailedJobNotifier\FailedJobNotifierService
+use GenTux\FailedJobNotifier\FailedJobNotifierService;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Queue;
+use Illuminate\Support\ServiceProvider;
+use PHPUnit\Framework\TestCase;
 
 class FailedJobNotifierServiceTest extends TestCase
 {
-	public function setUp()
+	/**
+     * @var Mockery\Mock
+     */
+	protected $app;
+	
+	/**
+     * @var ServiceProvider
+     */
+	protected $provider;
+	
+	protected function setUp()
 	{
 		parent::setUp();
 		
-		app()->register(FailedJobNotifierService::class);
+		$this->setUpMocks();
+		
+		$this->provider = new FailedJobNotifierService($this->app);
 	}
+	
+	protected function setUpMocks()
+    {
+        $this->app = Mockery::mock(Application::class);
+    }
+	
+	/**
+     * @test
+     */
+    public function it_can_be_constructed()
+    {
+        $this->assertInstanceOf(FailedJobNotifierService::class, $this->provider);
+    }
+	
+	/**
+     * @test
+     */
+    public function it_does_nothing_in_the_register_method()
+    {
+        $this->assertNull($this->provider->register());
+    }
+	
+	/**
+     * @test
+     */
+    public function it_does_boot()
+    {
+		Queue::spy();
+		
+		$this->provider->boot();
+    }
 }
