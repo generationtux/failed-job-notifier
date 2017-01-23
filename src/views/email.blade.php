c7d81cbb349de9c98a34def4ff31d7478904f481
Lark Mullins <larkmullins@gmail.com>
2017-01-23 14:12:57 -0500

    view specific view

---
+++ src/views/email.blade.php
@@ -0,0 +1,5 @@
+<h2>There is a new failed job in the Payments service.</h2>
+<p>To investigate further click the link below:</p>
+<p>
+    <a href="{{ getenv('PAYMENTS_URL') }}/healthz/failed-jobs">{{ getenv('PAYMENTS_URL') }}/healthz/failed-jobs</a>
+</p>
